43255446b0292d708c3c1bcd9e22fa0b7c2ef29e
Filipe Santos <filipess@live.com>
2021-02-21 15:22:30 -0300

    update readme and bump version to 1.0.1

--- packages/nativescript-pubnub/angular/index.ts
+++ packages/nativescript-pubnub/angular/index.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 import { PubNub } from '@groupsosimple/nativescript-pubnub';
 
-@Injectable()
+@Injectable({
+	providedIn: 'root',
+})
 export class PubNubService extends PubNub {}
